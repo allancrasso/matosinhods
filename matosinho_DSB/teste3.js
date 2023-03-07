1bc02f64978451149bec21af07d19de6b5b6e120
Allan Crasso <60273934+allancrasso@users.noreply.github.com>
2023-03-07 08:20:11 -0300

    Delete matosinho_DSB directory

--- matosinho_DSB/teste3.js
+++
@@ -1,9 +0,0 @@
-var nome = (prompt("Digite seu nome: "))
-var sobrenome = (prompt("Digite seu sobrenome: "))
-var idade = (prompt("Digite sua idade"))
-
-var nomeresultado = (nome +"\n "+ sobrenome + "\n tem " + idade + " anos")
-
-document.write(nomeresultado)
-alert(nomeresultado)
-console.log(nomeresultado)
--- matosinho_DSB/testelu2.js
+++
@@ -1,8 +0,0 @@
-var nome = prompt("Nome");
-var sobrenome = prompt("Sobrenome");
-var idade = prompt ("Idade");
-
-var mostrar = (nome+" "+sobrenome+" "+"tem\n"+idade+" anos");
-
-document.write(mostrar);
-alert (mostrar)
